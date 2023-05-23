09c59214371afac2282c37f2acacb67a1900baec
Malanga sandrine <57763741+sAndrin3@users.noreply.github.com>
2023-05-23 20:09:48 +0300

    Update App.jsx
    App.jsx conflict resolved.

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,82 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import About from "./pages/About";
-import Game from "./pages/Game";
+
+
 function App() {
-  const [Board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [Player, setPlayer] = useState("X");
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
+
+  const checkWinner = () => {
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    if (board.every((value) => value !== "")) {
+      return "draw";
+    }
+    return null;
+  };
 
   const changeValue = (idx) => {
-    setBoard(Board.map((value, index) => {
-      if (idx === index && value === "") {
-        return Player;
+    if (board[idx] === "" && !winner) {
+      setBoard((prevBoard) => {
+        const newBoard = [...prevBoard];
+        newBoard[idx] = player;
+        return newBoard;
+      });
+
+      const gameWinner = checkWinner();
+      if (gameWinner) {
+        setWinner(gameWinner);
+      } else {
+        setPlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
       }
-      return value;
-    }));
-    setPlayer(Player === "X" ? "O" : "X");
+    }
+  };
+
+  const resetBoard = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setPlayer("X");
+    setWinner(null);
   };
 
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Game/>} />
-        </Routes>
-        <div className="Wrapper">
-          <div className="Grid--board">
-            <span className="Grid--item" onClick={() => changeValue(0)}>{Board[0]}</span>
-            <span className="Grid--item" onClick={() => changeValue(1)}>{Board[1]}</span>
-            <span className="Grid--item" onClick={() => changeValue(2)}>{Board[2]}</span>
-            <span className="Grid--item" onClick={() => changeValue(3)}>{Board[3]}</span>
-            <span className="Grid--item" onClick={() => changeValue(4)}>{Board[4]}</span>
-            <span className="Grid--item" onClick={() => changeValue(5)}>{Board[5]}</span>
-            <span className="Grid--item" onClick={() => changeValue(6)}>{Board[6]}</span>
-            <span className="Grid--item" onClick={() => changeValue(7)}>{Board[7]}</span>
-            <span className="Grid--item" onClick={() => changeValue(8)}>{Board[8]}</span>
-          </div>
+    <div>
+      <h1 classname="heading"> Tic-tac-Toe</h1>
+      <div className="Wrapper">
+        <div className="Grid--board">
+          {board.map((value, index) => (
+            <span
+              key={index}
+              className="Grid--item"
+              onClick={() => changeValue(index)}
+            >
+              {value}
+            </span>
+          ))}
         </div>
-      </BrowserRouter>
-    </>
+        {winner && (
+          <div className="Result">
+            {winner === "draw" ? <p>It's a draw!</p> : <p>Winner: {winner}</p>}
+            <button onClick={resetBoard}>Restart</button>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
